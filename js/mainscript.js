0f917a8f6716a301ef4bbe24146fefe38c18e901
Mehedi Hasan <hasancoders0@gmail.com>
2024-08-25 04:51:41 +0600

    add primary button

---
+++ js/mainscript.js
@@ -0,0 +1,3 @@
+document.querySelectorAll('.btn-primary').forEach(function(element) {
+    element.insertAdjacentHTML('beforeend', '<span class="first"></span><span class="second"></span><span class="third"></span><span class="fourth"></span>');
+});
